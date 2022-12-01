5b3511241ef66364732a5cfc70963aebd096fcb4
EderA117 <ederalvarez117@gmail.com>
2022-11-30 19:25:58 -0600

    Se declaran las variables faltantes y se cambia el for por while

--- polino.cpp
+++ polino.cpp
@@ -3,21 +3,24 @@
 
 int main()
 {
-	int decision;
+	int decision,i;
+	float polinomio[12];
 	printf("Polinomios\nSe presenta un menu donde tendras solo 3 opciones\n");
 	printf("Para intoducir el ponimonio y valuarlo presione 1\nPara multiplicar 2 polinomios presione 2\nPara dividri dos polinomios presione 3\n");
 	scanf("%i",&decision);
 	switch(decision)
 	{
-	case 1;
-	for(i=0;i<=12;i++)
+	case 1:
+	i=0;
+	while(i<=12)
 			{
 				printf("Introduzca el coeficiente del polinomio: x^%i\n",i);				scanf("%f",&polinomio[i]);
+				i++;
 			}
 	break;
-	case 2;
+	case 2:
 	break;
-	case 3;
+	case 3:
 	break;
 	}
 }
