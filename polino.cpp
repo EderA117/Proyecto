eda275449e42cbf58ca9648cd01638596f3d93db
EderA117 <113396905+EderA117@users.noreply.github.com>
2022-11-30 18:11:11 -0600

    Add files via upload

---
+++ polino.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int 
+	printf("Polinomios\nSe presenta un menu donde tendras solo 3 opciones\n");
+	printf("Para intoducir el ponimonio y valuarlo presione 1\nPara multiplicar 2 polinomios presione 2\nPara dividri dos polinomios presione 3\n");
