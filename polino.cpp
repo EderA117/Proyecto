038a52a6158e1853504be8d515d597a3d4030b64
EderA117 <ederalvarez117@gmail.com>
2022-11-30 18:27:59 -0600

    Se agrega variable y se agrega menu

--- polino.cpp
+++ polino.cpp
@@ -3,6 +3,16 @@
 
 int main()
 {
-	int 
+	int decision;
 	printf("Polinomios\nSe presenta un menu donde tendras solo 3 opciones\n");
 	printf("Para intoducir el ponimonio y valuarlo presione 1\nPara multiplicar 2 polinomios presione 2\nPara dividri dos polinomios presione 3\n");
+	switch(decision)
+	{
+	case 1;
+	break;
+	case 2;
+	break;
+	case 3;
+	break;
+	}
+}
