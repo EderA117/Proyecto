5b77bc765947fb745cf49ca1e317ef3bb8e43f74
EderA117 <ederalvarez117@gmail.com>
2022-11-30 19:15:58 -0600

    Se agrega scanf y despues se aplica una condicion para mostrar y escanear los coeficientes del polinomio

--- polino.cpp
+++ polino.cpp
@@ -6,9 +6,14 @@ int main()
 	int decision;
 	printf("Polinomios\nSe presenta un menu donde tendras solo 3 opciones\n");
 	printf("Para intoducir el ponimonio y valuarlo presione 1\nPara multiplicar 2 polinomios presione 2\nPara dividri dos polinomios presione 3\n");
+	scanf("%i",&decision);
 	switch(decision)
 	{
 	case 1;
+	for(i=0;i<=12;i++)
+			{
+				printf("Introduzca el coeficiente del polinomio: x^%i\n",i);				scanf("%f",&polinomio[i]);
+			}
 	break;
 	case 2;
 	break;
