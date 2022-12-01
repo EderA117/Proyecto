f319cdc6e929dce7fe410a79b68389f143ad51f1
EderA117 <ederalvarez117@gmail.com>
2022-11-30 19:44:41 -0600

    Se agrega variable de tipo flotatnte y un print junto con scan donde se guarda el valor a valuar
    clear

--- polino.cpp
+++ polino.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	int decision,i;
-	float polinomio[12];
+	float polinomio[12],valor;
 	printf("Polinomios\nSe presenta un menu donde tendras solo 3 opciones\n");
 	printf("Para intoducir el ponimonio y valuarlo presione 1\nPara multiplicar 2 polinomios presione 2\nPara dividri dos polinomios presione 3\n");
 	scanf("%i",&decision);
@@ -17,6 +17,7 @@ int main()
 				printf("Introduzca el coeficiente del polinomio: x^%i\n",i);				scanf("%f",&polinomio[i]);
 				i++;
 			}
+			printf("Digite en que valor quiere valuar la funcion: \n");			scanf("%f",&valor);
 	break;
 	case 2:
 	break;
