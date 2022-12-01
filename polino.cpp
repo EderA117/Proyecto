e801cc2dfe8956a4ffd2e2f3b3aee7d9e9459e08
EderA117 <ederalvarez117@gmail.com>
2022-11-30 20:11:10 -0600

    Se hace lo mismo pero ahora para el segundo polinomio

--- polino.cpp
+++ polino.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-	int decision,i,f;
-	float polinomio[12],polinomio1[12];
+	int decision,i,f,g;
+	float polinomio[12],polinomio1[12],polinomio2[12];
 	printf("Polinomios\nSe presenta un menu donde tendras solo 3 opciones\n");
 	printf("Para intoducir el ponimonio y valuarlo presione 1\nPara multiplicar 2 polinomios presione 2\nPara dividri dos polinomios presione 3\n");
 	scanf("%i",&decision);
@@ -26,6 +26,10 @@ int main()
 		for(f=0;f<=12;f++)
 			{
 				printf("Introduzca los coeficientes del 1er polinomio: x^%i\n",f);				scanf("%f",&polinomio1[f]);
+			}
+			for(g=0;g<=12;g++)
+			{
+				printf("Introduzca los coeficientes del 2do polinomio: x^%i\n",g);				scanf("%f",&polinomio2[g]);
 			}
 	break;
 	case 3:
